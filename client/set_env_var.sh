a32d6b52ad2e8f0ff162648e4282c6b2b37fbf61
edwardszczepanski <eszczepa@uoregon.edu>
2017-05-11 21:42:38 -0700

    Tweaked env var setting

--- client/set_env_var.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-export oSAFARI_dir=`pwd`
--- client/setup_script.sh
+++ client/setup_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo crontab -l > my_cron_backup.txt 2>> log
-#export oSAFARI_dir=`pwd`
-. ./set_env_var.sh
+#./env_var.sh
 echo "* * * * * "`pwd`"/cron.sh" > cron_input
 sudo crontab cron_input
