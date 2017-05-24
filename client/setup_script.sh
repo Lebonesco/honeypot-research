aac348d813e60c03acc3b0181cbe051d266a9b1f
edwardszczepanski <eszczepa@uoregon.edu>
2017-05-23 22:42:54 -0700

    Working on cron script

--- client/setup_script.sh
+++ client/setup_script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# This line saves the current crontab
 sudo crontab -l > my_cron_backup.txt 2>> log
-#./env_var.sh
+# This line ensures that the file has the correct permissions
+chmod 777 cron.sh
+cat config.sh cron.sh > temp && mv temp cron.sh
+# This line setups the crontab file to be run
 echo "* * * * * "`pwd`"/cron.sh" > cron_input
-sudo crontab cron_input
+crontab cron_input
