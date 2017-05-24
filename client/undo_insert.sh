1890ae8f45f7e191e5a323a387b88f3204aa3308
edwardszczepanski <eszczepa@uoregon.edu>
2017-05-23 23:29:42 -0700

    finished undo insert script

--- client/undo_insert.sh
+++ client/undo_insert.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# THIS FILE FINDS THE NUMBER OF LINES IN THE CONFIG (THAT IS BEING INSERTED TO THE TOP OF THE CRON.SH
+# IT THEN RUNS THE SED COMMAND WITH DELETES THAT MANY LINES FROM CRON.SH
 wc config.sh > xxx
 read lines words characters filename < xxx 
-echo $lines
+variable="1,${lines}d"
+sed -e $variable < cron.sh > temp && mv temp cron.sh
 rm xxx
-#line_number=wc -l config.sh
-#sed -e '1,6d' < cron.sh > temp && mv temp cron.sh
