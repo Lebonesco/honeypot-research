5e6aa8ae095b45abcea66dbf30ca53d48ab4009b
edwardszczepanski <eszczepa@uoregon.edu>
2017-05-27 19:50:39 -0700

    adding tweepy to cron automated scripts

---
+++ client/model_config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+consumer_key=12341243
+consumer_secret=12341234
+access_token=12341234-3hxbXaaEGRCUivqZid0CgQWQDpTIvHYei5nM9VH
+access_token_secret=12341234
+twitter_handle=some_handle
+id=5
--- client/script.py
+++ client/script.py
@@ -1,8 +1,17 @@
+import tweepy
 import sys
+import random
 
 def main():
-    arg1 = str(sys.argv[1])
-    open(arg1 + ".txt", 'w')
+    #arg1 = str(sys.argv[1])
+    #open(arg1 + ".txt", 'w')
+    tweepy_testing(str(sys.argv[1], str(sys.argv[2]), str(sys.argv[3]), str(sys.argv[4]))
+
+def tweepy_testing(con_key, con_sec, acc_tok, acc_sec):
+    auth = tweepy.OAuthHandler(con_key, con_sec)
+    auth.set_access_token(acc_tok, acc_sec)
+    api = tweepy.API(auth)
+    api.update_status('Testing ' + str(random.random() * 10))
 
 if __name__ == '__main__':
     main()
