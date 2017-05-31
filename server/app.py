73d0597890ea62b201f6f47a5b6064140313d6a8
edward <edward@cs.uoregon.edu>
2017-05-31 11:28:59 -0700

    working API serverside

--- server/app.py
+++ server/app.py
@@ -1,20 +1,29 @@
 from flask import Flask, render_template, request, session, g, redirect, url_for, abort, flash
-import sys, pscopg2, json
-from config import dbname, dbhost, dbport
+import sys, psycopg2, json
+from config import dbname, dbhost, dbport, dbuser, dbpassword
 
 app = Flask(__name__)
 app.sercert_key = 'some_key'
 app.debug = True
 
-@app.route('/post_data', methods('POST',))
+@app.route('/post_data', methods=('POST',))
 def post_data():
-    conn = psycopg2.connect(dbname=dbname, host=dbhost, port=dbport)
-    cursors = conn.cursor()
-    if request.method == 'POST' and 'arguments' in request.form:
-        req = json.loads(request.form['arguments']
+    conn = psycopg2.connect(dbname=dbname, user=dbuser, password=dbpassword)
+    cursor = conn.cursor()
+    res = None
+    if request.method == 'POST' and request.args.get('input', '') != None:
+        #req = json.loads(request.form['arguments'])
+        input_value = str(request.args.get('input', ''))
+        query = "INSERT INTO data (value) VALUES (" + input_value + ");"
+        cursor.execute(query)
+        conn.commit()
+        res = "OK"
+    else:
+        res = "Please add a parameter for input"
 
-    # Do some work here
-    dat = None
-    data = json.dumps(dat)
+    data = json.dumps(res)
     conn.close()
     return data
+
+if __name__ == '__main__':
+    app.run(host='128.223.4.35', port=5555, debug=True)
